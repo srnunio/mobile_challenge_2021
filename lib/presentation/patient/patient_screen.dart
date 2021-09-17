6e967c2c0e33dacf57b579c640d20862ad5d4025
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2021-09-17 15:18:41 +0100

    improvements to patient_screen.dart

--- lib/presentation/patient/patient_screen.dart
+++ lib/presentation/patient/patient_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobile_challenge_2021/application/patient/patients_view_model.dart';
 import 'package:mobile_challenge_2021/injection/injection.dart';
+import 'package:mobile_challenge_2021/utils/strings_util.dart';
 
 class PatientScreen extends StatefulWidget {
   static const route = '/patient_screen';
@@ -15,6 +16,14 @@ class _PatientState extends State<PatientScreen>
     with SingleTickerProviderStateMixin {
   PatientsViewModel _model = inject<PatientsViewModel>();
 
+  String messageFailure() {
+    if (!_model.hasError) return '';
+    return _model.failure.when(
+        networkError: () => 'network'.translate,
+        emptyResult: () => 'no_results_found'.translate,
+        serverError: () => 'failure'.translate);
+  }
+
   _body() {
     if (_model.isBusy)
       return Center(
@@ -22,8 +31,48 @@ class _PatientState extends State<PatientScreen>
       );
 
     if (!_model.hasData || _model.hasError) {
-      return Column();
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Icon(
+            Icons.info_outline,
+            size: 55.0,
+          ),
+          Txt(
+            messageFailure(),
+            textAlign: TextAlign.center,
+          ),
+          InkWell(
+            onTap: _model.load,
+            child: Txt(
+              'try_again',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      );
     }
+
+    return ListView.builder(
+      itemBuilder: (ctx, index) {
+        var patient = _model.patients[index];
+        return Container(
+          child: Txt(
+            patient.name.first,
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
+      itemCount: _model.patients.length,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model.load();
   }
 
   @override
