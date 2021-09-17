edef05f7377a754e993b02d0431065957d5c1a91
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2021-09-17 13:12:38 +0100

    update register patient_repository

--- lib/injection/injection_config.dart
+++ lib/injection/injection_config.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile_challenge_2021/domain/core/i_navigation_service.dart';
+import 'package:mobile_challenge_2021/domain/patient/i_patient_repository.dart';
 import 'package:mobile_challenge_2021/infrastructure/core/navigation_service.dart';
 import 'package:mobile_challenge_2021/infrastructure/patient/patient_repository.dart';
 import 'package:mobile_challenge_2021/injection/register_module.dart';
@@ -30,7 +31,8 @@ class InjectorConfig {
   void registerViewModels() {}
 
   void registerRepositories() {
-    _injector.registerFactory(() => PatientRepository(_dio));
+    _injector
+        .registerFactory<IPatientRepository>(() => PatientRepository(_dio));
   }
 }
 
