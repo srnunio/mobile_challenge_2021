fa571abe6caaa43a48b7b2e15b440306c54c6f75
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2021-09-18 17:30:39 +0100

    custom page route

---
+++ lib/presentation/transparent_route.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TransparentRoute<T> extends PageRoute<T> {
+  TransparentRoute({
+    required this.builder,
+    RouteSettings? routeSettings,
+  })  :
+        super(settings: routeSettings, fullscreenDialog: false);
+
+  final WidgetBuilder builder;
+
+  @override
+  bool get opaque => false;
+
+  @override
+  Color get barrierColor => Colors.transparent;
+
+  @override
+  String? get barrierLabel => null;
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 350);
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    final widget = builder(context);
+    return FadeTransition(
+      opacity: Tween<double>(begin: 0, end: 1).animate(animation),
+      child: Semantics(
+        scopesRoute: true,
+
+        explicitChildNodes: true,
+        child: widget,
+      ),
+    );
+  }
+}
