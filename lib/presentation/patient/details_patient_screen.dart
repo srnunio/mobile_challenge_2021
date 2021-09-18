54a04599cf2ead8d72a897a97e19273e831d6285
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2021-09-18 19:34:14 +0100

    new keys translate -v3 update -v1 details_patient_screen

--- lib/presentation/patient/details_patient_screen.dart
+++ lib/presentation/patient/details_patient_screen.dart
@@ -1,7 +1,12 @@
 import 'package:customized/customized.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobile_challenge_2021/domain/core/i_navigation_service.dart';
 import 'package:mobile_challenge_2021/domain/patient/entities/patient.dart';
+import 'package:mobile_challenge_2021/injection/injection.dart';
+import 'package:mobile_challenge_2021/presentation/core/component/image/remote_image.dart';
+import 'package:mobile_challenge_2021/presentation/core/sizes.dart';
+import 'package:mobile_challenge_2021/presentation/core/styles.dart';
+import 'package:mobile_challenge_2021/utils/colors.dart';
 
 class DetailsPatientScreen extends StatefulWidget {
   static const route = '/details_patient_screen';
@@ -15,6 +20,10 @@ class DetailsPatientScreen extends StatefulWidget {
 
 class _DetailsPatientState extends State<DetailsPatientScreen>
     with SingleTickerProviderStateMixin {
+  var navigation = inject<INavigationService>();
+
+  void _close() => navigation.navigateToPop();
+
   @override
   void initState() {
     super.initState();
@@ -22,8 +31,67 @@ class _DetailsPatientState extends State<DetailsPatientScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (_) {
-      return Scaffold(body: empty);
-    });
+    var size = MediaQuery.of(context).size;
+    var height = size.height * (3 / 4);
+    return Scaffold(
+        backgroundColor: Colors.black45,
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Positioned(
+              bottom: 0.0,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                width: size.width,
+                height: height,
+                color: kBackgroundColor,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.clear,
+                          color: kPrimaryColor2,
+                        ),
+                        onPressed: _close,
+                      ),
+                    ),
+                    verticalSpaceLarge(),
+                    Txt(
+                      widget.patient.fullName,
+                      textStyle: (_) =>
+                          styleText(fontWeight: FontWeight.bold, size: 28.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    Txt(
+                      widget.patient.email,
+                      textAlign: TextAlign.center,
+                    ),
+                    Txt(
+                      widget.patient.phone,
+                      textAlign: TextAlign.center,
+                      textStyle: (_) =>
+                          styleText(fontWeight: FontWeight.bold,color: kPrimaryColor3),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 100.0,
+              child: Container(
+                decoration: decoration(
+                    border: Border.all(color: kBackgroundColor, width: 4.0),
+                    shape: BoxShape.circle),
+                child: RemoteImage(
+                  urlImage: widget.patient.picture,
+                  width: 150.0,
+                  height: 150.0,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
